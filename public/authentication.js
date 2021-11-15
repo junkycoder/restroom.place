dbb8d1554e188e816d9b2d082f008babdfcc2758
Dan Hromada <hromada.dan@gmail.com>
2021-11-15 12:18:48 +0100

    Confirm magic link and check current user
    caution: currentUser is asynchrounously set and isCurrentUserVerified returns false first few ms
    
    Drop some styles aswell

--- public/authentication.js
+++ public/authentication.js
@@ -4,12 +4,24 @@ import {
   onAuthStateChanged,
   signInAnonymously,
   sendSignInLinkToEmail,
+  isSignInWithEmailLink,
+  signInWithEmailLink,
 } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-auth.js";
 
 export const auth = getAuth(app);
 
-export function isCurrentUserVerified() {
-  return auth.currentUser?.emailVerified;
+export let currentUser = window.currentUser = auth.currentUser;
+
+onAuthStateChanged(auth, (user) => {
+  window.currentUser =currentUser = user;
+});
+
+/**
+ * Use only in async handlers, this tooks a while to load
+ * @returns {Promise<void>}
+ */
+export async function isCurrentUserVerified() {
+  return currentUser && currentUser.emailVerified === true;
 }
 
 export async function sendMagicLink({
@@ -17,7 +29,7 @@ export async function sendMagicLink({
   domain = "restroom.place",
   destination = "/",
 } = {}) {
-  const url = `https://${domain}${destination}`;
+  const url = `https://${domain}${destination || ""}`;
   console.info(`Sending magic link to ${email}`, url);
 
   if (!email) throw new Error("Email is required");
@@ -29,6 +41,42 @@ export async function sendMagicLink({
     handleCodeInApp: true,
   };
 
-  await sendSignInLinkToEmail(auth, email, settings)
+  await sendSignInLinkToEmail(auth, email, settings);
   window.localStorage.setItem("emailForSignIn", email);
+}
+
+export function isMagicLink(link) {
+  return isSignInWithEmailLink(auth, link || window.location.href);
+}
+
+export async function confirmMagicLink(link) {
+  if (!link) link = window.location.href;
+
+  if (!isMagicLink(link)) {
+    throw new Error("Not a magic link");
+  }
+
+  // Additional state parameters can also be passed via URL.
+  // This can be used to continue the user's intended action before triggering
+  // the sign-in operation.
+  // Get the email if available. This should be available if the user completes
+  // the flow on the same device where they started it.
+  let email = window.localStorage.getItem("emailForSignIn");
+  if (!email) {
+    // User opened the link on a different device. To prevent session fixation
+    // attacks, ask the user to provide the associated email again. For example:
+    email = window.prompt("Prosím doplňte e-mail pro potvrzení.");
+    window.localStorage.setItem("emailForSignIn", email);
+  }
+  // The client SDK will parse the code from the link for you.
+  const result = await signInWithEmailLink(auth, email, link);
+  console.info("Signed in user with result", result);
+
+  // Clear email from storage.
+  window.localStorage.removeItem("emailForSignIn");
+  // You can access the new user via result.user
+  // Additional user info profile not available via:
+  // result.additionalUserInfo.profile == null
+  // You can check if the user is new or existing:
+  // result.additionalUserInfo.isNewUser
 }
