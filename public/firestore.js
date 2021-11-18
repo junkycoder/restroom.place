1ddeb9329140d61314f3fe8ff2e8a25485e904c6
Dan Hromada <hromada.dan@gmail.com>
2021-11-18 07:00:39 +0100

    Validation and reading data from db

--- public/firestore.js
+++ public/firestore.js
@@ -4,11 +4,19 @@ import {
   collection,
   getDocs,
   connectFirestoreEmulator,
+  doc,
+  getDoc,
 } from "https://www.gstatic.com/firebasejs/9.4.1/firebase-firestore.js";
 import { isLocalhost } from "/library.js";
+import "/authentication.js";
 
 export const db = getFirestore(app);
 
 if (isLocalhost()) {
-  connectFirestoreEmulator(db, "localhost", 5001);
+  connectFirestoreEmulator(db, "localhost", 8080);
+}
+
+export async function getDocData(path) {
+  const snap = await getDoc(doc(db, path));
+  return snap.data();
 }
