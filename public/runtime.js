25faed7737d290f8e97a572faed35f3735bf3d6a
Dan Hromada <hromada.dan@gmail.com>
2021-11-19 09:47:05 +0100

    Check magic link on every screen

--- public/runtime.js
+++ public/runtime.js
@@ -11,3 +11,32 @@ const checkTouchScreen = () => {
 };
 setTimeout(checkTouchScreen, 0);
 window.document.addEventListener("DOMContentLoaded", checkTouchScreen);
+
+import {
+  isMagicLink,
+  confirmMagicLink,
+  isCurrentUserVerified,
+} from "/authentication.js";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  try {
+    if (isMagicLink()) {
+      await confirmMagicLink();
+      window.location.search = "";
+    }
+  } catch (error) {
+    console.error(error);
+
+    if (error.code === "auth/invalid-action-code") {
+      alert("Neplatný odkaz, zkuste to znovu.");
+
+      window.location.href = `/verify-self?destination=${encodeURIComponent(
+        wiondow.location.pathname
+      )}`;
+    } else {
+      alert("Nastala chyba.");
+    }
+  } finally {
+    console.timeEnd("User verification");
+  }
+});
