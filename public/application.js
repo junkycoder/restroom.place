694d2e2d4c5a642f918e84240d22c2da94fea92d
Dan Hromada <hromada.dan@gmail.com>
2021-11-22 09:04:53 +0100

    Add analytics

--- public/application.js
+++ public/application.js
@@ -1,5 +1,6 @@
 // Note: Imports has to be from https://www.gstatic.com/, otherwise COmponents like auth and firestore wont work together
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/9.5.0/firebase-analytics.js";
 
 const firebaseConfig = {
   apiKey: "AIzaSyBeH_RDEDJj_63xSc3LGrCvgMiCV3dT-Tg",
@@ -8,7 +9,9 @@ const firebaseConfig = {
   storageBucket: "restroom-place.appspot.com",
   messagingSenderId: "746979184762",
   appId: "1:746979184762:web:f9f3767b16f9dedf0febc3",
+  measurementId: "G-YBMXH9H78K",
 };
 
 export const app = initializeApp(firebaseConfig);
 
+export const analytics = getAnalytics(app);
