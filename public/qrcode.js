0a83745c9afc9639eda03c508d6000a3847c706e
Dan Hromada <hromada.dan@gmail.com>
2021-12-04 07:25:29 +0100

    Allow qr print for anonymous users
    An fix missing library

--- public/qrcode.js
+++ public/qrcode.js
@@ -1,12 +1,15 @@
-(() => {
-  const link = "https://unpkg.com/qrcode-generator@1.4.4/qrcode.js";
-  const script = document.createElement("script");
-  script.src = link;
-  document.head.appendChild(script);
-})();
+
 
 export function createQrCode(roomId) {
-  const code = qrcode(4, "L");
+  if (!window.qrcode) {
+    console.error("QR code library not loaded");
+    console.warn(`Add this to before your </head>
+      <script src="https://unpkg.com/qrcode-generator@1.4.4/qrcode.js"></script>
+      (because it does not supports ESM)
+    `);
+  }
+
+  const code = window.qrcode(4, "L");
   code.addData(`https://restroom.place/room/${roomId}?fe`);
   code.make();
   return code.createSvgTag(8, 24);
